751fd5827f10a532f986fbea3585867072b3ed08
Carmen Almonacid Martos <carmen@Carmens-MacBook-Pro.local>
2022-06-13 16:50:04 +0200

    routes

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AlbumsComponent } from './pages/albums/albums.component';
+import { BandComponent } from './pages/band/band.component';
+import { FormComponent } from './pages/form/form.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path: "", pathMatch: "full", component: HomeComponent},
+{ path: "Band", component: BandComponent},
+{ path: "Albums", component: AlbumsComponent },
+{ path: "Form", component: FormComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
